91977ca03acc0a561871d0c2b6f9352fee81f949
ab409 <409308624@qq.com>
2015-01-06 21:43:36 +0800

    get edge version 2

--- getEdge.m
+++ getEdge.m
@@ -1,17 +1,56 @@
 function result = getEdge(image)
 n = 5;
 [height, width] = size(image);
+%%
+%version 1
+% tmp = double(zeros(height, width));
+% 
+% for i = 1 : n
+%    core(i) = strel('diamond', i);
+% end
+% 
+% for i = 2 : n
+%     tmp0 = double(getEdgeCore(core(i), image));
+%     tmp0 = double(imerode(tmp0, core(i - 1)));
+%     tmp = tmp + tmp0;
+% end
+% result = uint8(tmp / (n - 1));
+%%
+%version 2
+% core = strel('diamond', 3);
+% 
+% result = getEdgeCore(core, image);
+%%
+%version 3
 tmp = double(zeros(height, width));
 
 for i = 1 : n
    core(i) = strel('diamond', i);
 end
 
-for i = 2 : n
+for i = 1 : n
     tmp0 = double(getEdgeCore(core(i), image));
-    tmp0 = double(imerode(tmp0, core(i - 1)));
     tmp = tmp + tmp0;
 end
-result = uint8(tmp / (n - 1));
+result = uint8(tmp / n);
+%%
+%version 4
+% core1 = [1 0; 0 1; 0 0; 1 0; 0 1];
+% core(:, :, 1) = core1;
+% for i = 2 : n
+%    core(:, :, i) = imdilate(core(:, :, i-1), core1);
+% end
+% tmp = double(zeros(height, width));
+% for i = 2 : n
+%     tmp0 = double(getEdgeCore(core(:, :, i), image));
+%     tmp0 = double(imerode(tmp0, core(:, :, i - 1)));
+%     tmp = tmp + tmp0;
+% end
+% result = uint8(tmp / (n - 1));
+
+
+
+
+
 
 
