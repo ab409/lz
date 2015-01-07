1df1265067b336e250b58fa8c2c082296807a2c1
guangyuanyu <guangyuanyu@sohu-inc.com>
2015-01-07 11:17:16 +0800

    segment version 2

--- getNextKey.m
+++ getNextKey.m
@@ -1,20 +1,41 @@
 function result = getNextKey(image, key)
-
+%%
+%%
 [height, width] = size(image);
-back = zeros(height, width);
-target = zeros(height, width);
+% back = zeros(height, width);
+% target = zeros(height, width);
+% for i = 1 : height 
+%    for j = 1 : width
+%       if image(i, j) < key
+%          back(i, j) = image(i, j);
+%          target(i, j) = 0;
+%       else
+%          back(i, j) = 255;
+%          target(i, j) = image(i, j);
+%       end
+%    end
+% end
+% backMean = mean(back(:));
+% targetMean = mean(target(:));
+% result = (backMean + targetMean) / 2;
+
+%%
+%
+backVal = 0.0;
+targetVal = 0.0;
+backCount = 0;
+targetCount = 0;
 for i = 1 : height 
    for j = 1 : width
       if image(i, j) < key
-         back(i, j) = image(i, j);
-         target(i, j) = 0;
+         backVal = backVal + image(i, j);
+         backCount = backCount + 1;
       else
-         back(i, j) = 255;
-         target(i, j) = image(i, j);
+         targetVal = targetVal + image(i, j);
+         targetCount = targetCount + 1;
       end
    end
 end
-backMean = mean(back(:));
-targetMean = mean(target(:));
+backMean = backVal / backCount;
+targetMean = targetVal / targetCount;
 result = (backMean + targetMean) / 2;
-
