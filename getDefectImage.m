35aaa2817b13ea1c2788361d778bfe10fbc6572e
ab409 <409308624@qq.com>
2015-01-08 21:03:09 +0800

    modify no defect found

--- getDefectImage.m
+++ getDefectImage.m
@@ -7,11 +7,11 @@ for i = 1 : width
     line(:, 1) = 255;
     [x, y] = getExtremePoint(image(:, i));
     [up, down] = getDefect(x, y, line);
-%     if up ~= 1 && down ~= 1
-%         a = up 
-%         b = down
-%     end
-    line(up :down, 1) = image(up : down, i); 
+     if up == 1 && down == 1
+        line(:, 1) = 255;
+     else
+        line(up :down, 1) = image(up : down, i);
+     end    
     newImage(:, i) = line;
 end
 newImage = uint8(newImage);
